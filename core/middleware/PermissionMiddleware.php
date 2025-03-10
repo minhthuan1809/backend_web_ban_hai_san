5cbad3ff5a4053df6d8afa6b406caf703a9d5a48
minhthuan1809 <thuan1809minh@gmail.com>
2025-03-10 09:12:58 +0700

    phan quyen

--- core/middleware/PermissionMiddleware.php
+++ core/middleware/PermissionMiddleware.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . '/../controller/permission/PermissionController.php';
+require_once __DIR__ . '../../controller/permission/PermissionController.php';
 
 class PermissionMiddleware {
     private $permissionController;
